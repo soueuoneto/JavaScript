5f6289c7db7af2a7da39a6027f77860da1b226f7
unknown <ARISTEU0737@digitalcollegesul.local>
2024-09-30 08:40:22 -0300

    variaveis

---
+++ aula01/1_variaveis.js
@@ -0,0 +1,17 @@
+// delcaração de variáveis
+
+let idade = 19;
+var isGamer = false;
+const nome = 'carlos';
+
+// redeclaração
+
+let idade = 30;
+var isGamer = true;
+const nome = 'João';
+
+// reatribuição
+
+idade = 29;
+isGamer = false;
+nome = 'Marcos'
