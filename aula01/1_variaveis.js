50ee705b5e2fa680593d37d9c1cd637d093e7e47
soueuoneto <aneto.corp99@gmail.com>
2024-09-30 09:07:23 -0300

    marcação com readme

--- aula01/1_variaveis.js
+++ aula01/1_variaveis.js
@@ -1,17 +1,43 @@
-// delcaração de variáveis
+// declaração de variáveis
 
-let idade = 19;
-var isGamer = false;
-const nome = 'carlos';
+// let idade = 19;
+// var isGamer = false;
+// const nome = 'Carlos';
+// console.log(idade);
 
 // redeclaração
 
-let idade = 30;
-var isGamer = true;
-const nome = 'João';
+// let idade = 30; // Erro: Não permite redeclaração
+// var isGamer = true;
+// const nome = 'João'; // Erro: Não permite redeclaração
 
 // reatribuição
 
-idade = 29;
-isGamer = false;
-nome = 'Marcos'
+// console.log(idade);
+// idade = 29;
+// isGamer = false;
+// nome = 'Marcos';
+
+// Hoisting (Içamento / elevação) somente var
+
+// salgado = 9;
+// console.log(salgado, pratinho);
+// var pratinho = 15;
+
+// var salgado = 9;
+// console.log(salgado, pratinho);
+// var pratinho = 15;
+
+// Possíveis maneiras de declarar variáveis
+
+let name123;
+let Name123;
+let $name123;
+let _name123;
+
+// Não dá pra fazer
+
+// let 123Name123;
+// let @123name;
+// let "name";
+// let 'name';
