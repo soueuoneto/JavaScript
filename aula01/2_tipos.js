4f56ec87722262423cfecf032d92a3e0ac938876
soueuoneto <aneto.corp99@gmail.com>
2024-09-30 10:54:25 -0300

    tipos

---
+++ aula01/2_tipos.js
@@ -0,0 +1,17 @@
+// Primitivos
+
+typeof 10;
+typeof 'carlos';
+typeof true;
+typeof null;
+typeof undefined;
+
+// Métodos para number
+
+(10).toFixed(2);
+parseInt(10,50);
+parseFloat(10,6);
+
+// Métodos para strings
+('JavaScript').toUpperCase();
+
